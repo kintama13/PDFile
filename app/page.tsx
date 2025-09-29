f8acd7417fa9b01491a311a1581c4a024a4297f9
kintama13 <11201059@student.itk.ac.id>
2025-09-29 14:36:13 +0800

    Header design done

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>Bilal</h1>
-      <Button variant={"outline"} size={"lg"}>Tombol Random</Button>
+    <div className="">
+      <Button variant={"outline"} size={"lg"}>
+        Tombol Random
+      </Button>
     </div>
   );
 }
